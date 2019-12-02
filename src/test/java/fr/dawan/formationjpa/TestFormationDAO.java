ad840c9ea3cfbfb5bfeab8377c4da084d70084a2
helenou <ln.lejeune@gmail.com>
2019-12-02 15:05:40 +0100

    assert one record

--- src/test/java/fr/dawan/formationjpa/TestFormationDAO.java
+++ src/test/java/fr/dawan/formationjpa/TestFormationDAO.java
@@ -1,14 +1,18 @@
 package fr.dawan.formationjpa;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+
+import fr.dawan.formationjpa.dao.GenericDAO;
+import fr.dawan.formationjpa.entities.Formation;
 
 public class TestFormationDAO {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void findByIdTest() {
+		Formation f= GenericDAO.findById(Formation.class, 3);  // look for db id 3
+		assertNotNull(f);
 	}
 
 }
