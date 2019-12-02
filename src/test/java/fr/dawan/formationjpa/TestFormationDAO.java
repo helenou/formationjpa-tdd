ce6884988af173daacc006f959099d027916c590
helenou <ln.lejeune@gmail.com>
2019-12-02 14:50:38 +0100

    adding test repo

---
+++ src/test/java/fr/dawan/formationjpa/TestFormationDAO.java
@@ -0,0 +1,14 @@
+package fr.dawan.formationjpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFormationDAO {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
